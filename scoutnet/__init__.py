ae7010a51bd46b7c4dd8de5805ba3ffe546ec35b
Jakob Schlyter <jakob@kirei.se>
2024-06-16 16:21:31 +0200

    update

--- scoutnet/__init__.py
+++ scoutnet/__init__.py
@@ -1,5 +1,10 @@
 from importlib.metadata import version
 
-from .client import ScoutnetClient  # noqa
+from .client import (  # noqa
+    ScoutnetClient,
+    ScoutnetMailinglist,
+    ScoutnetMailinglistMember,
+    ScoutnetMember,
+)
 
 __version__ = version("scoutnet")
